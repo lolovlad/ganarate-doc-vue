fd59f90525c5edf99eb658c2e87a342d5908a97f
lolovlad <lolovlad33@gmail.com>
2024-05-10 15:11:43 +0400

    add dis

--- src/components/Forms/PrefabFieldForm.vue
+++ src/components/Forms/PrefabFieldForm.vue
@@ -4,7 +4,7 @@
       <form class="col s12" @submit.prevent>
         <div class="row" v-for="field in fields" :key="field.name">
           <div class="input-field col s12">
-            <input :placeholder="field.name" :type="field.type" v-model="field.val">
+            <input :placeholder="field.discription" :type="field.type" v-model="field.val">
           </div>
         </div>
         <AgreeButton @click="$emit('saveChange', fields, uuidPrefab)">Сохранить</AgreeButton>
